8b94b00c815754afc92ebfd9762e1b6b73c87ffc
Miguel Elias <elias.msalomao.miguel599@gmail.com>
2022-12-11 14:45:10 -0300

    add metodo get para classe client parking

--- src/main/java/com/api/parkingcontrol/controllers/ClientParkingController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ClientParkingController.java
@@ -2,8 +2,12 @@ package com.api.parkingcontrol.controllers;
 
 import com.api.parkingcontrol.dtos.ClientParkingDto;
 import com.api.parkingcontrol.models.ClientParkingModel;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ClientParkingService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import javax.validation.Valid;
 import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -28,6 +34,20 @@ public class ClientParkingController {
         BeanUtils.copyProperties(clientParkingDto, clientParkingModel);
         clientParkingModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.status(HttpStatus.CREATED).body(clientParkingService.save(clientParkingModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllClientParking(@PageableDefault(page = 0, size = 5, sort = "id",direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(clientParkingService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneClientParking(@PathVariable(value = "id") UUID id){
+        Optional<ClientParkingModel> clientParkingModelOptional = clientParkingService.findById(id);
+        if (!clientParkingModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client Parking not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(clientParkingModelOptional.get());
     }
 
 
--- src/main/java/com/api/parkingcontrol/services/ClientParkingService.java
+++ src/main/java/com/api/parkingcontrol/services/ClientParkingService.java
@@ -2,9 +2,12 @@ package com.api.parkingcontrol.services;
 
 import com.api.parkingcontrol.models.ClientParkingModel;
 import com.api.parkingcontrol.repositories.ClientParkingRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ClientParkingService {
@@ -18,5 +21,13 @@ public class ClientParkingService {
     @Transactional
     public Object save(ClientParkingModel clientParkingModel) {
         return clientParkingRepository.save(clientParkingModel);
+    }
+
+    public Object findAll(Pageable pageable) {
+        return clientParkingRepository.findAll(pageable);
+    }
+
+    public Optional<ClientParkingModel> findById(UUID id) {
+        return clientParkingRepository.findById(id);
     }
 }
